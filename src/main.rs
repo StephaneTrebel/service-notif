bf2457ec8e63519c878e7160271d308744ecf86b
Stéphane TRÉBEL (WeScale) <stephane.trebel@wescale.fr>
2025-04-13 21:30:46 +0200

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short = 't', long = "toto")]
+    toto: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("toto: {}", args.toto);
 }
