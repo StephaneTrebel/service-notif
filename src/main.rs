6268e465db52db2cb97af5def89f7b8960c942bf
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-04-20 20:33:30 +0200

    Add infinite loop with customizable interval

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+use std::{
+    thread::sleep,
+    time::Duration,
+};
+
 use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(short = 't', long = "toto")]
-    toto: String,
+    #[arg(short = 'i', long = "interval")]
+    interval: u64,
 }
 
 fn main() {
     let args = Cli::parse();
+    println!("Starting...");
 
-    println!("toto: {}", args.toto);
+    let interval: Duration = Duration::from_millis(args.interval);
+
+    loop {
+        println!("interval: {}", args.interval);
+        sleep(interval);
+    }
 }
