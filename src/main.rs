e8c1f939427948032095ebd99993dc0cb6384d6e
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2025-04-20 20:46:59 +0200

    Add customizable URL for fetching with reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
-use std::{
-    thread::sleep,
-    time::Duration,
-};
+use std::{thread::sleep, time::Duration};
 
 use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(short = 'i', long = "interval")]
+    #[arg(long = "interval")]
     interval: u64,
+
+    #[arg(long = "url")]
+    url: String,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     println!("Starting...");
 
     let interval: Duration = Duration::from_millis(args.interval);
+    let url: String = args.url;
 
     loop {
-        println!("interval: {}", args.interval);
+        println!("Fetching URL: {}", &url);
+
+        let res = reqwest::blocking::get(&url)?;
+        println!("Response: {:?}, {}", res.version(), res.status());
+
         sleep(interval);
     }
 }
